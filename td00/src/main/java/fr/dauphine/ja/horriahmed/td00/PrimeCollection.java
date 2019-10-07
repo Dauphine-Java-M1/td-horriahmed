06b25aacfbdccb1075c96fea60005e59f5a6b6cb
horri <horriahmed97@gmail.com>
2019-10-07 15:12:50 +0200

    initail commit

---
+++ td00/src/main/java/fr/dauphine/ja/horriahmed/td00/PrimeCollection.java
@@ -0,0 +1,53 @@
+package fr.dauphine.ja.horriahmed.td00;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class PrimeCollection 
+{
+    ArrayList<Integer> numbers;
+    
+    public PrimeCollection() {
+		numbers =new ArrayList<Integer>();	}
+    
+    public void initRandom(int n,int m){
+    	Random random = new Random();
+    	
+    	for( int i=0;i<n;i++) {
+    		int randomInt =random.nextInt(m);
+    		numbers.add(randomInt);
+    	}
+    }
+    
+    public boolean isPrime(int p) {
+    	
+    	if(p==1 || p==0)
+    		return false;
+    	
+    	else {
+    	for(int i=2; i<Math.sqrt(p); i++) {
+    		if(p%i==0) return false;
+    	}
+    	
+    	return true;
+    	}
+ }
+    
+    public void printPrime() {
+    	for( int i=0;i<numbers.size();i++) {
+    		if(isPrime(i))
+    			System.out.println(i);
+    	}
+    }
+    
+    public static void main(String[] args) {
+		
+    	PrimeCollection p =new PrimeCollection();
+    	p.initRandom(10, 100);
+    	p.printPrime();
+
+    
+    }
+}
+
+
