6a6f6cf526866d4e44383669d092a79085227446
horri <horriahmed97@gmail.com>
2019-10-21 14:07:19 +0200

    EXO 1:Point

---
+++ shapes/src/main/java/fr/dauphine/ja/horriahmed/shapes/Point.java
@@ -0,0 +1,39 @@
+package fr.dauphine.ja.horriahmed.shapes;
+
+public class Point {
+	private double x;
+	private double y;
+	public static int nombrePoints=0;
+	
+	/*public Point() {
+		this.x=0.0;
+		this.y=0.0;
+	}
+	
+	
+*/	
+	public Point(double px, double py) {
+		this.x=px;
+		this.y=py;
+	nombrePoints++;	
+	}
+
+	public Point(Point p) {
+		this.x=p.x;
+		this.y=p.y;
+	nombrePoints++;	
+	}
+	
+ public double getX() {
+	 return x;
+ }
+ 
+ public double getY() {
+	 return y;
+ }
+
+ public String toString() {
+	 return "("+this.x+","+this.y+")";
+ }
+}
+
---
+++ shapes/src/main/java/fr/dauphine/ja/horriahmed/shapes/TestPoint.java
@@ -0,0 +1,13 @@
+package fr.dauphine.ja.horriahmed.shapes;
+
+public class TestPoint {
+
+	 public static void main(String[] args) {
+			
+		 Point p =new Point(0.0,0.0);
+	     System.out.println(p.getX()+""+p.getY());
+	     System.out.println(p);
+	    
+	  	     
+	  }
+}
